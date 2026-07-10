d0f56865c3936d0871378e84e8f1b89ef04c8921
yogaxzie <yogasyahputra111111@gmail.com>
2026-07-10 12:57:53 +0700

    Create _init_.py

---
+++ modules/_init_.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from . import osint_tiktok
+from . import osint_instagram
+from . import osint_whatsapp
+from . import osint_name
+from . import osint_ip
+from . import hacking_scan
+from . import tools_hash
+from . import tools_password
+from . import tools_encrypt
+from . import fun_quote
+from . import fun_fact
+from . import system_info
+
+__all__ = [
+    'osint_tiktok', 'osint_instagram', 'osint_whatsapp',
+    'osint_name', 'osint_ip', 'hacking_scan',
+    'tools_hash', 'tools_password', 'tools_encrypt',
+    'fun_quote', 'fun_fact', 'system_info'
+]
